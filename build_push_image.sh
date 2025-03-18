1cf6240a8652ad013525542ed37b79a0460abf96
frieren-chan <suhrideveloper@gmail.com>
2025-03-18 13:23:24 +0700

    Create build_push_image.sh

---
+++ build_push_image.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Define variables
+IMAGE_NAME="item-app"
+TAG="v1"
+GITHUB_USERNAME="frieren-chan"
+
+# Build Docker image
+docker build -t $IMAGE_NAME:$TAG .
+
+# List images
+docker images
+
+# Rename image for GHCR (GitHub Container Registry)
+docker tag $IMAGE_NAME:$TAG ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME:$TAG
+
+# Login to GHCR
+echo $GHCR_TOKEN | docker login ghcr.io -u $GITHUB_USERNAME --password-stdin
+
+# Push image to GHCR
+docker push ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME:$TAG
